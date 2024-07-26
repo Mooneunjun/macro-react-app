c2e59adaf99ba5786172d08285145ee9018595a0
Mooneunjun <mooneunjun@icloud.com>
2024-07-26 23:49:04 +0900

    no message

--- src/components/Share/Tooltip.js
+++ src/components/Share/Tooltip.js
@@ -3,6 +3,7 @@ import "./Tooltip.css";
 
 const Tooltip = ({ className, text }) => {
   return <div className={`tooltip ${className}`}>{text}</div>;
+  // div 태그에 className을 props로 받아와서 적용합니다.
 };
 
 export default Tooltip;
