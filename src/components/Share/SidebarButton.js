b2f06ead57501defa1e60842c7320bf9f2f31222
Mooneunjun <mooneunjun@icloud.com>
2024-07-30 23:58:25 +0900

    툴팁 정리/개선

--- src/components/Share/SidebarButton.js
+++ src/components/Share/SidebarButton.js
@@ -1,50 +1,31 @@
-import React, { useEffect, useRef } from "react";
-import tippy from "tippy.js";
+import React from "react";
+import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css"; // optional
 import "./MyTooltip.css";
 import "tippy.js/dist/border.css";
 
 const SidebarButton = ({ toggleSidebar, className, isSidebarOpen }) => {
-  const buttonRef = useRef(null);
   const tooltipText = isSidebarOpen ? "사이드바 닫기" : "사이드바 열기";
 
-  useEffect(() => {
-    const button = buttonRef.current;
-
-    let instance;
-
-    if (button) {
-      instance = tippy(button, {
-        content: tooltipText,
-        placement: "bottom-start",
-        theme: "black",
-      });
-    }
-
-    return () => {
-      if (instance) {
-        instance.destroy();
-      }
-    };
-  }, [tooltipText]);
-
   return (
-    <button ref={buttonRef} className={className} onClick={toggleSidebar}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <path
-          fill="currentColor"
-          fillRule="evenodd"
-          d="M3 8a1 1 0 0 1 1-1h16a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1m0 8a1 1 0 0 1 1-1h10a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1"
-          clipRule="evenodd"
-        ></path>
-      </svg>
-    </button>
+    <Tippy content={tooltipText} placement="bottom-start" theme="black">
+      <button className={className} onClick={toggleSidebar}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            fillRule="evenodd"
+            d="M3 8a1 1 0 0 1 1-1h16a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1m0 8a1 1 0 0 1 1-1h10a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </Tippy>
   );
 };
 
--- src/components/Share/Tooltip.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Tooltip.css";
-
-const Tooltip = ({ className, text }) => {
-  return <div className={`tooltip ${className}`}>{text}</div>;
-};
-
-export default Tooltip;
