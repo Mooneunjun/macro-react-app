8d11b760fe201c52f9ae3593a79c53f69c57de61
Mooneunjun <mooneunjun@icloud.com>
2024-08-06 23:51:03 +0900

    no message

--- src/components/Share/SettingsPopup.js
+++ src/components/Share/SettingsPopup.js
@@ -68,7 +68,7 @@ const SettingsPopup = ({ className, closeSettingsPopup }) => {
             }`}
             ref={themeSelectRef}
           >
-            <div className="them-select-option-item" value="default">
+            <div className="them-select-option-item elect" value="default">
               <span>시스템</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -84,10 +84,34 @@ const SettingsPopup = ({ className, closeSettingsPopup }) => {
               </svg>
             </div>
             <div className="them-select-option-item" value="dark">
-              다크 모드
+              <span>다크 모드</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                  clipRule="evenodd"
+                />
+              </svg>
             </div>
-            <div className="them-select-option-item" value="light">
-              라이트 모드
+            <div className="them-select-option-item " value="light">
+              <span>라이트 모드</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                  clipRule="evenodd"
+                />
+              </svg>
             </div>
           </div>
         </div>
